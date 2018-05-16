45caa7efb1aba4bf790bda458f2c8f7c48728d61
Luis Caceres <luimacaroster@gmail.com>
2018-05-16 16:19:14 -0400

    prueba de insercion de datos en bdd

--- SiembraSmart/routes/users.js
+++ SiembraSmart/routes/users.js
@@ -1,5 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "siembraSmart"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Conectado a la Base de Datos!");
+  
+  var sql = "INSERT INTO localidades (localizationID, region, ciudad, longitud_mapa, latitud_mapa) VALUES (000, 'Metropolitana', Santiago, -33.462805, -70.660001)";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 localidad insertada");
+  });
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
